23388c557e2700e0d3a5a93f9ec4de8733dacde6
flo62134 <florentbrassart31@gmail.com>
2020-05-22 00:33:25 +0200

    Completed stage 2

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,3 +1,13 @@
 # Write your code here
-print('H A N G M A N')
-print('The game will be available soon.')
+FAILURE = 'You are hanged!'
+SUCCESS = 'You survived!'
+ASK_FOR_INPUT = 'Guess the word:'
+TITLE = 'H A N G M A N'
+
+word = 'python'
+print(TITLE)
+attempt = input(ASK_FOR_INPUT)
+if attempt == word:
+    print(SUCCESS)
+else:
+    print(FAILURE)
